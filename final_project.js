de1e17677d0b1a459ccb96a91bfc931298ebda48
Anahit K <kostikyan.ana@gmail.com>
2017-05-02 15:00:38 -0700

    hi

--- final_project.js
+++ final_project.js
@@ -1,10 +1,10 @@
 var namespace = "http://www.w3.org/2000/svg"
 
 // Write your code here!
-
-
-makeImage("http://rogerbaxter.com/JapaneseBeetle/Beetle_Page_3_files/Beetle_Colorized.gif",75,30,60,60)
+makeImage("http://rogerbaxter.com/JapaneseBeetle/Beetle_Page_3_files/Beetle_Colorized.gif",220,30,60,60)
 // DO NOT EDIT CODE BELOW THIS LINE!
+
+
 function getX(shape) {
   if (!shape) {
     throw "Uh oh, you tried to get the x coordinate of a shape that doesn't exist!"
